bf5b02e62794fb96c44385513a854a3520ea77ff
Maik Vlcek <maik@mediavrog.net>
2016-10-18 13:55:28 +0900

    support github reviews

--- src/background.js
+++ src/background.js
@@ -1,44 +1,70 @@
 // When the extension is installed or upgraded ...
 chrome.runtime.onInstalled.addListener(function () {
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostContains: 'github.com'}
-          })
-        ],
-        // And shows the extension's page action.
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }
-    ]);
-  });
+    // Replace all rules ...
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        // With a new rule ...
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: {hostContains: 'github.com'}
+                    })
+                ],
+                // And shows the extension's page action.
+                actions: [new chrome.declarativeContent.ShowPageAction()]
+            }
+        ]);
+    });
 });
 
 chrome.pageAction.onClicked.addListener(function (event) {
-  //console.log("Page action:", event);
-  lgtm();
+    //console.log("Page action:", event);
+    lgtm();
 });
 
 chrome.commands.onCommand.addListener(function (command) {
-  //console.log('Command:', command);
-  lgtm(command);
+    //console.log('Command:', command);
+    lgtm(command);
+});
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    // console.log("message received", message);
+    if (message.action == 'loadLgtm') {
+        loadLgtm(function (lgtmResponse) {
+            var payload = {action: 'initiate', type: message.type, lgtm: lgtmResponse};
+            sendMessage(payload);
+        })
+    }
 });
 
 function lgtm(command) {
-  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    var payload = {action: 'initiate', type: command ? command : 'lgtm'};
+    // console.log('sending', payload);
+    sendMessage(payload);
+}
+
+function sendMessage(payload) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        if(tabs[0]) chrome.tabs.sendMessage(tabs[0].id, payload);
+    });
+}
+
+function loadLgtm(callback) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://www.lgtm.in/g", true);
+    xhr.open("GET", "http://www.lgtm.in/g?PageSpeed=noscript", true);
     xhr.setRequestHeader('Accept', 'application/json');
     xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-        var data = JSON.parse(xhr.responseText);
-        //console.log("Received data: ", data);
-        chrome.tabs.sendMessage(tabs[0].id, {"type": command ? command : "lgtm", "data": data});
-      }
+        if (xhr.readyState == 4 && callback) {
+            var lgtm = {};
+            try {
+                lgtm = JSON.parse(xhr.responseText);
+                //console.log("Received lgtm: ", lgtm);
+            } catch (e) {
+                console.warn(e);
+            }
+
+            callback(lgtm);
+        }
     };
     xhr.send();
-  });
 }
--- src/lgtm.js
+++ src/lgtm.js
@@ -1,31 +1,91 @@
 // LGTM
+var lgtmLoadingPlaceholder = "# Loading LGTM.. #";
+var lgtmNoSubmitType = "lgtm_no_submit";
+
 chrome.runtime.onMessage.addListener(
     function (message, sender, sendResponse) {
-      if (message && message.data) {
-        var input = document.getElementById("new_comment_field");
+        // console.log(message);
+        if (message) {
+            if (message.lgtm) message.lgtm['summary'] = "LGTM!\n" + message.lgtm['markdown'];
+            // message.type = lgtmNoSubmitType; // debug
 
-        // input field is present
-        if (input) {
-          input.value += "LGTM!\n" + message.data['markdown'];
-
-          // also submit directly
-          if (message.type != "lgtm_no_submit") {
-            console.log('LGTMed! ', message.data['imageUrl']);
-            var submitButtonWrap = document.getElementById("partial-new-comment-form-actions");
-            if (submitButtonWrap) {
-              var submitButtonCandidates = submitButtonWrap.getElementsByClassName("btn-primary");
-              if (submitButtonCandidates.length > 0) submitButtonCandidates[0].click()
-            }
-          }
+            var wasSubmitted = submitAsComment(message) || submitAsReview(message);
+            if (wasSubmitted && message.lgtm) console.log('LGTMed! with', message.lgtm['imageUrl']);
         }
-      }
     }
 );
 
-//chrome.runtime.onMessage.addListener(
-//    function (message, sender, sendResponse) {
-//      var input = document.getElementById("new_comment_field");
-//      input.innerHTML += message.data;
-//      console.log('LGTMed!');
-//    }
-//);
+/**
+ * Github Plain old comment style
+ * @param message
+ * @returns {boolean} If the form was actually submitted after entering the LGTM.
+ */
+function submitAsComment(message) {
+    var input = document.getElementById("new_comment_field");
+    if (input) {
+        handleMessage(input, message);
+
+        if (message.type != lgtmNoSubmitType) {
+            var submitBtn = document.querySelector("#partial-new-comment-form-actions .btn-primary");
+            if (submitBtn) {
+                submitBtn.click();
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+/**
+ * Github new reviews style. Will set the review status to 'approve' before filling in the LGTM.
+ * @param message
+ * @returns {boolean} If the form was actually submitted after entering the LGTM.
+ */
+function submitAsReview(message) {
+    var reviewToggle = document.querySelector(".js-reviews-container > button.js-menu-target");
+    if (reviewToggle) {
+        // need to open the review
+        var expandedAttr = reviewToggle.attributes.getNamedItem('aria-expanded');
+        if (!expandedAttr || expandedAttr.nodeValue == 'false') reviewToggle.click();
+
+        // select the 'approve' state
+        var approve = document.querySelector("#submit-review[aria-expanded=true] input[value=approve]");
+        if (approve) approve.click();
+
+        // set lgtm
+        var input = document.getElementById("pull_request_review_body");
+        handleMessage(input, message);
+
+        if (message.type != lgtmNoSubmitType) {
+            var submitBtn = document.querySelector("#submit-review[aria-expanded=true] [type=submit]");
+            if (submitBtn) {
+                submitBtn.click();
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+function handleMessage(input, message) {
+    if (message.lgtm && message.lgtm['summary']) {
+        setLoading(input, false);
+        input.value += message.lgtm['summary']
+    } else {
+        setLoading(input, true);
+        loadLgtm(message.type);
+    }
+}
+
+function setLoading(input, isLoading) {
+    var ph = lgtmLoadingPlaceholder;
+
+    input.value = input.value.replace(new RegExp(ph, 'g'), '');
+    if (isLoading) input.value += ph;
+}
+
+function loadLgtm(type) {
+    chrome.runtime.sendMessage({action: 'loadLgtm', type: type});
+}
